320444a6109c64776de30ac7680616963526b8d3
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-11 00:00:27 -0400

    Demonstrate import nuances

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,3 +3,19 @@
 
 def enlarge(n):
     return n * 100
+
+# print("JUNK")
+# print("GLOBAL SCOPE")
+#
+# y = float(input("PLEASE INPUT A NUMBER TO ENLARGE: "))
+# print(enlarge(y))
+
+if __name__ == "__main__":
+    # only if run from the command line, invoke the following code:
+    # otherwise, don't
+
+    print("JUNK")
+    print("GLOBAL SCOPE")
+
+    y = float(input("PLEASE INPUT A NUMBER TO ENLARGE: "))
+    print(enlarge(y))
