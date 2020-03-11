cd1a5498d2468f004d5ff798eac8fb7981fc837b
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-10 23:52:47 -0400

    Import function from mod

--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -3,7 +3,13 @@
 
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
+
 print("HAPPY TUESDAY NIGHT.")
 
 df = pandas.DataFrame({"x":[1,2,3], "y":[4,5,6]})
 print(df.head())
+
+x = 5
+print("ENLARGE", x, "TO", enlarge(x))
