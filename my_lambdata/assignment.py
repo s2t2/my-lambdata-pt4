f4ed780bf416e391ab7069b1b1df23200be5efb5
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-16 22:47:38 -0400

    Implement desired functionality

--- my_lambdata/assignment.py
+++ my_lambdata/assignment.py
@@ -2,19 +2,34 @@
 
 import pandas
 
-# State abbreviation -> Full Name and visa versa. FL -> Florida, etc.
-# (Handle Washington DC and territories like Puerto Rico etc.)
-# take a pandas dataframe that has state abbreviations
-# ... and write a function to add corresponding state names
+def add_state_names(my_df):
+    """
+    Adds corresponding state names to a dataframe.
 
-def add_state_names():
-    pass
-
+    Param: my_df (pandas.DataFrame) containing a column called "abbrev"
+    """
+    new_df = my_df.copy()
+    # type(new_df["abbrev"]) #> <class 'pandas.core.series.Series'>
+    # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
+    names_map = {
+        "CA": "Cali",
+        "CT": "Conn",
+        "CO": "Colorado"
+        # todo: more abbrevs!
+    }
+    new_df["name"] = new_df["abbrev"].map(names_map)
+    return new_df
 
 if __name__ == "__main__":
 
+    print("--------------")
     df = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
     print(df.head())
+    new_df = add_state_names(df)
+    print(new_df.head())
 
+    print("--------------")
     df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA"]})
     print(df2.head())
+    new_df2 = add_state_names(df2)
+    print(new_df2.head())
