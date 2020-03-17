d44d4b8fbf6df29c56e6de41f58aea8b21318afb
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-16 23:22:01 -0400

    Use inheritance approach

--- my_lambdata/assignment.py
+++ my_lambdata/assignment.py
@@ -1,21 +1,16 @@
-# my_lambdata/assignment.py (OOP approach)
+# my_lambdata/assignment.py (OOP approach w/ inheritance)
 
 import pandas
 
-class DataProcessor():
-    def __init__(self, my_df):
-        """
-        Param: my_df (pandas.DataFrame) containing a column called "abbrev"
-        """
-        self.df = my_df.copy()
+class CustomFrame(pandas.DataFrame):
+    """
+    Param: my_df (pandas.DataFrame) containing a column called "abbrev"
+    """
 
     def add_state_names(self):
         """
         Adds corresponding state names to a dataframe.
-
-        Param: my_df (pandas.DataFrame) containing a column called "abbrev"
         """
-        #new_df = self.df.copy()
         # type(new_df["abbrev"]) #> <class 'pandas.core.series.Series'>
         # see: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.map.html
         names_map = {
@@ -24,19 +19,41 @@ class DataProcessor():
             "CO": "Colorado"
             # todo: more abbrevs!
         }
-        self.df["name"] = self.df["abbrev"].map(names_map)
+        self["name"] = self["abbrev"].map(names_map)
 
 if __name__ == "__main__":
 
     print("--------------")
-    df1 = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+    #df1 = pandas.DataFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
     #print(df1.head())
     #new_df = add_state_names(df1)
     #print(new_df.head())
-    processor = DataProcessor(df1)
-    print(processor.df.head())
-    processor.add_state_names()
-    print(processor.df.head())
+
+    #processor = DataProcessor(df1)
+    #print(processor.df.head())
+    #processor.add_state_names()
+    #print(processor.df.head())
+
+    custom_df = CustomFrame({"abbrev": ["CA", "CT", "CO", "TX", "DC"]})
+    print(custom_df.head())
+    custom_df.add_state_names()
+    print(custom_df.head())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     #print("--------------")
     #df2 = pandas.DataFrame({"abbrev": ["OH", "MI", "CO", "TX", "PA"]})
